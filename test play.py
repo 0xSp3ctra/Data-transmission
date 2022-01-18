3fc5df243707ad3092805339499af0ab5c86fc97
Shelovescolin <84533475+Shelovescolin@users.noreply.github.com>
2022-01-18 14:48:12 +0100

    Update test play.py

--- test play.py
+++ test play.py
@@ -1,15 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import sounddevice as sd
-from math import pi
+A = 0.5 #amplitude
+f = 1000 #fréquence
+fs = 100000 #échantillonage
+t = np.arange(0, 10, 1/fs) #temps
 
-fs = 41000
+signal = A * np.cos(f * 2 * np.pi * t) #sign
+    
+fig = plt.figure(figsize=(12, 6))
+ax = plt.axes()
+ax.plot(t,signal)
+plt.xlim(0, 0.003)
 
-data = np.arange(0, 5, 1/fs)
-
-f = 650
-
-
-x = np.sin(2*pi*f*data)
-
-sd.play(x, fs, blocking=True)
-
+stockSignal = np.array(signal)
+sd.play(stockSignal, fs, loop=True)
