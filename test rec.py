7d410bb601ba6ae8d377bd5d145c18e6c1bbb7b2
Shelovescolin <84533475+Shelovescolin@users.noreply.github.com>
2022-01-18 10:58:18 +0100

    Update test rec.py

--- test rec.py
+++ test rec.py
@@ -1,34 +1,32 @@
 import matplotlib.pyplot as plt
 import sounddevice as sd
-from scipy.fttpack import ftt
+from scipy.fftpack import fft
 import numpy as np
 plt.close('all')
+ax = plt.axes()
 
-fs = 16000
-d = 3
+fs = 44100
+d = 5
 
 print("Start recording")
 
 a = sd.rec(int(d * fs), fs, 1, blocking=True)
+# Useless
+a = a.flatten()
+#
+print(a)
 
 print("End recording")
 
-sd.play(a, fs)
-plt.plot(a); plt.title("Recorded sound")
+plt.plot(a)
+plt.title("Recorded sound")
 
-# spectre
-x_t = fft(a)
+X_f = fft(a)
 
-# axes de fréquence
-x_f = np.size(a)
-fr = (Fs / 2) * np.linspace(0, 1, round(n/2))
-X_m = (2 / n) * abs(X_f([0,np.size(fr)]))
+ax.set_xlabel('Temps (s)')
+ax.set_ylabel('Amplitude')
 
-# figure du spectre
-plt.figure()
-plt.plot(fr, X_m)
-plt.xlabel("Fréquence (Hz)")
-plt.ylabel("Magnitude")
-plt.title(Spectre)
-
-
+n = np.size(a)
+fr = (fs / 2) * np.linspace(0, 1, round(n/2))
+X_m = (2 / n) * abs(X_f[0:np.size(fr)])
+plt.show()
